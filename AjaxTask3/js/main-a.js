c3782d289c5d14b42b6635e13592d567904ba6de
Oheneba <ohenebap@metropolia.fi>
2018-11-23 13:19:10 +0200

    task 4

--- AjaxTask3/js/main-a.js
+++ AjaxTask3/js/main-a.js
@@ -1,6 +1,7 @@
 // Create function 'showImages' which
 // adds the loaded HTML content to <ul> element
 
+/*
 const showImages = (html)=> {
   document.querySelector('ul').innerHTML=html;
 };
@@ -10,3 +11,20 @@ fetch('images.html').then((response) => {
 }).then((html)=> {
   showImages(html);
 });
+*/
+
+const showImages= (html)=>{
+  document.querySelector('ul').innerHTML=html;
+}
+
+
+
+fetch('images.html').then(
+    (response)=> {
+  return response.text();
+    }
+).then(
+    (html)=> {
+  showImages(html);
+    }
+  );
--- Task4ImageUpload/js/main_b.js
+++ Task4ImageUpload/js/main_b.js
@@ -13,3 +13,37 @@
 // function ends
 
 // make an event listener which calls upload function when the form is submitted
+
+
+// select input element where type is file
+const input = document.querySelector('#pic');
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit',e=>{
+ e.preventDefault();
+  // make FormData -object
+  const data = new FormData();
+// add file to FormData -object.
+// Note that 'files' is an FileList object. This means that you can upload multiple files.
+  data.append('fileToUpload', input.files[0]);
+  // make an object for settings
+  const settings = {
+    method: 'POST',
+    // credentials: 'same-origin', // this might be needed for some servers
+    body: data
+  };
+
+
+  // initiate fetch. In this example the server responds with text.
+// Response could also be json. Then you would use response.json()
+  fetch('/node/taskUploadImage', settings).then((response) => {
+    console.log(response);
+  });
+
+
+});
+
+
+
+
